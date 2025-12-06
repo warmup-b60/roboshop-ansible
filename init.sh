8e16dff1f76b2d974a340b8fba14e5ce09b725a7
CodingManoj <CodingManoj@gmail.com>
2025-12-06 18:25:58 -0500

    Provisioning V1 Infra

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Running ansible playbook for ${env} and ${component}" 
-ansible-playbook -i env/${env}/hosts roboshop.yml -e "component=${component}" -e "env=${env}" -e ansible_user=ec2-user -e ansible_password=DevOps321  roboshop.yml 
-echo "Running ansible playbook for ${env} and ${component} completed" 
+echo "Running ansible playbook for ${1} and ${2}" 
+ansible-playbook -i ${$1}-${$2}.roboshop.internal, -e "component=${1}" -e "env=${2}" -e ansible_user=ec2-user -e ansible_password=DevOps321  roboshop.yml 
+echo "Running ansible playbook for ${1} ${2} completed" 
